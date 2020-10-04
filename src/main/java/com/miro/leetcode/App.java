13e57ec460f33765e23a9747938f45e01599a09c
Miroslav Trninic <miroslav.trninic@gmail.com>
2020-10-04 19:34:01 +0200

    push

--- src/main/java/com/miro/leetcode/App.java
+++ src/main/java/com/miro/leetcode/App.java
@@ -4,7 +4,8 @@ public class App {
     public static void main(String... args) {
         TwoSum twoSum = new TwoSum();
         int[] nums = {1, 2, 3, 4};
-        System.out.println(twoSum.find(nums, 5));
+        //System.out.println(twoSum.find(nums, 5));
+        System.out.println("hello leetcode");
 
     }
 }
